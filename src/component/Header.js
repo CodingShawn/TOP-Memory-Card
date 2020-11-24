6ea3bd619361e398d8fa25c4b3d84e2d5b361b47
Shawn <codingshawn@gmail.com>
2020-11-24 21:13:39 +0800

    Did basic Header component

---
+++ src/component/Header.js
@@ -0,0 +1,12 @@
+import react from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Memory Card Game</h1>
+      <div>Try to click all 12 cards just once!</div>
+    </header>
+  );
+};
+
+export default Header;
